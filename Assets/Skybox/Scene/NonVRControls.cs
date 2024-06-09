e097da5ec42f12d8e1895424d31aed1972743b80
padamGueyeRobotist <pagueye@robotistgroup.com>
2024-06-09 13:39:34 +0000

    first push

---
+++ Assets/Skybox/Scene/NonVRControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class NonVRControls : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed = 200.0f;
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
